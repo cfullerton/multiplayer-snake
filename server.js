35bff360e29ec909f53efa6d073175f577df40a4
Conner Fullerton <conner0987@hotmail.com>
2017-03-10 23:47:26 -0600

    sessions end, added modules to repo

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(server);
 var locationData = {};
 var players = [];
 io.sockets.on('connection', function(socket) {
+  console.log(socket)
   socket.on('locationUpdate', function(data) {
     if (!locationData[data.sendID] || !(locationData[data.sendID] == data.sendDirection)){
     locationData[data.sendID]= data.sendDirection;
@@ -15,8 +16,17 @@ io.sockets.on('connection', function(socket) {
    players.push(data);
  })
   socket.on('idRegister', function(data) {     // needs to validate that no two are the same
-    console.log(data);
+    socket.playerID = data;
 
+  })
+  socket.on('disconnect', function() {
+    console.log(socket.playerID);
+    for (var i = 0; i <players.length;i++){
+      if (players[i].id == socket.playerID){
+        players.splice(i, 1);
+      }
+    }
+    delete locationData[socket.playerID];
   })
   socket.on('start',function() {
     io.sockets.emit('start', players);
