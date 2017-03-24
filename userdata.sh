52923b5b3ab636afe249933904a85420777cb8be
Conner <conner0987@hotmail.com>
2017-03-23 23:09:29 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,8 @@
-sudo yum update
-y
+yes | sudo yum update
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 4.4.5
-sudo yum install git
-y
+yes | sudo yum install git
 git clone https://github.com/cfullerton/multiplayer-snake.git
 cd multiplayer-snake/
 node server
