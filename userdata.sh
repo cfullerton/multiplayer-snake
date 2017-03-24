18a6cfbc4bcbbe67270f66d6bb7f70ddba5d4eff
Conner <conner0987@hotmail.com>
2017-03-23 22:26:23 -0500

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,10 @@
+yum update
+y
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 4.4.5
+sudo yum install git
+y
+git clone https://github.com/cfullerton/multiplayer-snake.git
+cd multiplayer-snake/
+node server
